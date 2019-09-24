c69622ea48c3d2a3ffd9af0d7a9e286fafb57cd8
韦海晖 <franciswei98@gmail.com>
2019-09-24 16:12:35 +0800

    basic timeline cal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,56 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <timeline ref="timeline" :items="items" :groups="groups" :options="options"></timeline>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  data: () => ({
+    groups: [],
+    items: [],
+    options: {
+      editable: true,
+    },
+  }),
+  created() {
+    const now = moment()
+      .minutes(0)
+      .seconds(0)
+      .milliseconds(0);
+    const groupCount = 4;
+    const itemCount = 20;
+    // create a data set with groups
+    const names = ['Frank', 'Island', 'Lee', 'Grant'];
+    for (let g = 0; g < groupCount; g += 1) {
+      this.groups.push({
+        id: g,
+        content: names[g],
+      });
+    }
+    // create a dataset with items
+    for (let i = 0; i < itemCount; i += 1) {
+      const start = now.clone().add(Math.random() * 200, 'hours');
+      const group = Math.floor(Math.random() * groupCount);
+      this.items.push({
+        id: i,
+        group,
+        content: `item ${i} <span style="color:#97B0F8;">(${names[group]})</span>`,
+        start,
+        type: 'box',
+      });
+    }
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import { Timeline } from 'vue2vis';
+import 'vue2vis/dist/vue2vis.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.component('timeline', Timeline);
 
 new Vue({
   router,
