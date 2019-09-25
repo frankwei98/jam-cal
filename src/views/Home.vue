0748cfaceb24e7b9f58bfb619663ac16e25561f3
韦海晖 <franciswei98@gmail.com>
2019-09-25 20:43:07 +0800

    删掉 HelloWorld 组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <timeline ref="timeline" :items="items" :groups="groups" :options="options"></timeline>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import moment from 'moment';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    groups: [],
+    items: [],
+    options: {
+      editable: true,
+    },
+  }),
+  created() {
+    const now = moment()
+      .minutes(0)
+      .seconds(0)
+      .milliseconds(0);
+    const groupCount = 4;
+    const itemCount = 20;
+    // create a data set with groups
+    const names = ['Frank', 'Island', 'Lee', 'Grant'];
+    for (let g = 0; g < groupCount; g += 1) {
+      this.groups.push({
+        id: g,
+        content: names[g],
+      });
+    }
+    // create a dataset with items
+    for (let i = 0; i < itemCount; i += 1) {
+      const start = now.clone().add(Math.random() * 200, 'hours');
+      const group = Math.floor(Math.random() * groupCount);
+      this.items.push({
+        id: i,
+        group,
+        content: `item ${i} <span style="color:#97B0F8;">(${names[group]})</span>`,
+        start,
+        type: 'box',
+      });
+    }
   },
 };
 </script>
