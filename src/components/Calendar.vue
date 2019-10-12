79ffe4cec46d032e6929ace36afd51f1ae793fae
韦海晖 <franciswei98@gmail.com>
2019-10-12 16:22:27 +0800

    feat: at least a workable calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,11 +3,16 @@
     <div class="loading" v-if="isLoading">
       <h1 class="title">Loading</h1>
     </div>
-    <div class="loaded" v-else>{{events}}</div>
+    <div class="loaded" v-else>
+      <p>日历的ID： {{calendarId}}</p>
+      <p>日历目前出现了: {{setOfAttendees.join(', ')}}</p>
+      <timeline ref="timeline" :items="items" :groups="groups" :options="options"></timeline>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
 import { getCalendar } from '../util/backend';
 
 export default {
@@ -15,6 +20,9 @@ export default {
   props: ['towerUserId', 'calendarId'],
   data: () => ({
     isLoading: true,
+    options: {
+      editable: false,
+    },
     calendar: {
       CALSCALE: 'GREGORIAN',
       PRODID: 'icalendar-ruby',
@@ -39,17 +47,53 @@ export default {
       console.error(error);
     }
   },
+  methods: {
+    getNameFromEventSummary(event) {
+      const ends = event.SUMMARY.indexOf(']');
+      return event.SUMMARY.slice(1, ends);
+    },
+    getTitleFromSummary(event) {
+      const afterName = event.SUMMARY.indexOf(']') + 1;
+      return event.SUMMARY.slice(afterName);
+    },
+  },
   computed: {
     events() {
-      return this.calendar.VEVENT;
+      const events = this.calendar.VEVENT;
+      const afterPostProcess = events.map((event) => {
+        const creator = this.getNameFromEventSummary(event);
+        const title = this.getTitleFromSummary(event);
+        return { ...event, creator, title };
+      });
+      return afterPostProcess;
     },
     setOfAttendees() {
-      const result = this.events.reduce((list, event) => {
-        const ends = event.SUMMARY.indexOf(']');
-        const name = event.SUMMARY.slice(1, ends);
-        return [...list, name];
-      }, []);
+      const result = this.events.reduce(
+        (list, event) => [...list, this.getNameFromEventSummary(event)],
+        [],
+      );
       return [...new Set(result)]; // a set
+    },
+    groups() {
+      return this.setOfAttendees.map((name, idx) => ({
+        id: idx,
+        content: name,
+      }));
+    },
+    items() {
+      return this.events.map((event, idx) => {
+        const attendee = this.groups.find(
+          member => this.getNameFromEventSummary(event) === member.content,
+        );
+        return {
+          id: idx, // seq
+          group: attendee.id, // belongs to which member
+          content: event.title,
+          start: moment(event.DTSTART),
+          end: moment(event.DTEND),
+          type: 'box',
+        };
+      });
     },
   },
 };
