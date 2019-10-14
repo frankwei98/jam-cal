5d3ef0f3f9dae732733906ee3e4b60b4444c6fd1
韦海晖 <franciswei98@gmail.com>
2019-10-14 22:44:46 +0800

    style: readjust

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = {
     '@vue/app',
   ],
   plugins: [
-    ['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: 'css' }],
+    ['import',
+      { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: 'css' },
+    ],
   ],
 };
